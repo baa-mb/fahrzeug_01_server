da15e4a1e7635dd7a78bc503c067d202e632e8b7
baa <34665120+baa-mb@users.noreply.github.com>
2021-09-26 19:50:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, 180)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P0, 90)
@@ -8,24 +15,24 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString.includes("A")) {
         control.raiseEvent(
         EventBusSource.MICROBIT_ID_BUTTON_A,
-        EventBusValue.MICROBIT_BUTTON_EVT_DOWN
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
     }
     if (receivedString.includes("B")) {
         control.raiseEvent(
         EventBusSource.MICROBIT_ID_BUTTON_B,
-        EventBusValue.MICROBIT_BUTTON_EVT_DOWN
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
     }
     if (receivedString.includes("AB")) {
         control.raiseEvent(
         EventBusSource.MICROBIT_ID_BUTTON_AB,
-        EventBusValue.MICROBIT_BUTTON_EVT_DOWN
+        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
     }
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 0)
 })
-basic.showIcon(IconNames.SmallDiamond)
+basic.showIcon(IconNames.Diamond)
 radio.setGroup(54)
