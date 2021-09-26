3701115ed52c3d564c8dea1b52a646f50ff9f077
baa <34665120+baa-mb@users.noreply.github.com>
2021-09-26 19:55:30 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,23 +13,25 @@ input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P0, 90)
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString.includes("A")) {
-        control.raiseEvent(
-        EventBusSource.MICROBIT_ID_BUTTON_A,
-        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-        )
-    }
-    if (receivedString.includes("B")) {
-        control.raiseEvent(
-        EventBusSource.MICROBIT_ID_BUTTON_B,
-        EventBusValue.MICROBIT_BUTTON_EVT_CLICK
-        )
-    }
     if (receivedString.includes("AB")) {
         control.raiseEvent(
         EventBusSource.MICROBIT_ID_BUTTON_AB,
         EventBusValue.MICROBIT_BUTTON_EVT_CLICK
         )
+    } else {
+        if (receivedString.includes("A")) {
+            control.raiseEvent(
+            EventBusSource.MICROBIT_ID_BUTTON_A,
+            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+            )
+        } else {
+            if (receivedString.includes("B")) {
+                control.raiseEvent(
+                EventBusSource.MICROBIT_ID_BUTTON_B,
+                EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+                )
+            }
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
